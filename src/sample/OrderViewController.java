5b6d9589e0c7477c1136836bf9a60e0cb9c646ae
Naeem <1705014@ugrad.cse.buet.ac.bd>
2019-02-02 22:33:28 +0600

    ClientIsClean...

--- src/sample/OrderViewController.java
+++ src/sample/OrderViewController.java
@@ -22,6 +22,7 @@ public class OrderViewController implements Initializable {
     @FXML Label OrderLabel;
     Socket socket;
     ArrayList<Customer> OrderList;
+    private int count;
 
     public OrderViewController() throws IOException {
     }
@@ -29,24 +30,37 @@ public class OrderViewController implements Initializable {
     @FXML public void DoneButtonClicked()
    {
 
+
    }
     @FXML public void NextOrderButtonClicked()
     {
 
+        if(count+1<OrderList.size()){
+            count++;
+            OrderLabel.setText(OrderList.get(count).toMessage());
+        }
     }
+    public void PreviousOrderButtonClicked(ActionEvent event) {
 
+        if(count-1>=0)
+        {
+            count--;
+            OrderLabel.setText(OrderList.get(count).toMessage());
+        }
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
        //networking needed to read from the arraylist of orders
+        count=0;
         try {
             socket=new Socket("localhost",4444);
             ObjectOutputStream outtoServer=new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream oinfromServer=new ObjectInputStream(socket.getInputStream());
             outtoServer.writeObject("Show Orders");
             OrderList= (ArrayList<Customer>) oinfromServer.readObject();
-            System.out.println(OrderList.get(0).toString());
-            OrderLabel.setText(OrderList.get(0).toString());
+            System.out.println(OrderList);
+            OrderLabel.setText(OrderList.get(count).toMessage());
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -55,8 +69,6 @@ public class OrderViewController implements Initializable {
         }
     }
 
-    public void PreviousOrderButtonClicked(ActionEvent event) {
-    }
 
     public void GoBackButtonClicked(ActionEvent event) throws IOException {
         Parent newsceneparent= FXMLLoader.load(getClass().getResource("OwnerIn.fxml"));
